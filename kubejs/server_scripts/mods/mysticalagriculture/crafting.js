3885d677c37ab2420c0e205486103f598bb2c494
André Augusto <andreaugustoqueiroz999@gmail.com>
2023-03-15 10:34:48 -0300

    ⬆️ updates
    ⬆️ update mods:
        🔷 AdAstra
        🔷 Additional Enchanted Miner
        🔷 Advanced Netherite
        🔷 AttributeFix
        🔷 Baubley Heart Canisters
        🔷 ChoiceTheorem's Overhauled Village
        🔷 Collective
        🔷 Create Craft & Additions
        🔷 Cyclops Core
        🔷 Dungeon Crawl
        🔷 Gardens of the Dead
        🔷 Geode+
        🔷 Handcrafted
        🔷 Integrated Dynamics
        🔷 Inventory Profiles Next
        🔷 libIPN
        🔷 Modonomicon
        🔷 Occultism
        🔷 Perpetual Durability
        🔷 ProjectE Integration
        🔷 Repurposed Structures
        🔷 Supplementaries
        🔷 Vampirism
        🔷 Xtra Arrows
    ➖ removed Pluto

--- kubejs/server_scripts/mods/mysticalagriculture/crafting.js
+++ kubejs/server_scripts/mods/mysticalagriculture/crafting.js
@@ -1,48 +1,51 @@
-ServerEvents.recipes(event =>
-{
+ServerEvents.recipes((event) => {
   // essence crafting for custom seeds
-  function essenceCircle(result, essenceType)
-  {
-    event.shaped(result, ['aaa', 'a a', 'aaa'], { a: `mysticalagriculture:${essenceType}_essence` }).id(`kubejs:mysticalagriculture/${essenceType}_essence_crafting`)
+  function essenceRecipe(result, material) {
+    event
+      .shaped(result, ["AAA", "A A", "AAA"], {
+        A: `mysticalagriculture:${material}_essence`,
+      })
+      .id(`kubejs:mysticalagriculture/${material}_essence_crafting`);
   }
-  essenceCircle('allthemodium:allthemodium_nugget', 'allthemodium')
-  essenceCircle('allthemodium:vibranium_nugget', 'vibranium')
-  essenceCircle('allthemodium:unobtainium_nugget', 'unobtainium')
-  essenceCircle('6x silentgear:azure_silver_ingot', 'azure_silver')
-  essenceCircle('6x silentgear:crimson_iron_ingot', 'crimson_iron')
+
+  essenceRecipe("allthemodium:allthemodium_nugget", "allthemodium");
+  essenceRecipe("allthemodium:vibranium_nugget", "vibranium");
+  essenceRecipe("allthemodium:unobtainium_nugget", "unobtainium");
+  essenceRecipe("6x silentgear:azure_silver_ingot", "azure_silver");
+  essenceRecipe("6x silentgear:crimson_iron_ingot", "crimson_iron");
 
   //magical soil crafting
   event.custom({
-    type: 'mysticalagriculture:infusion',
-    input: { item: 'mysticalagradditions:insanium_farmland' },
+    type: "mysticalagriculture:infusion",
+    input: { item: "mysticalagradditions:insanium_farmland" },
     ingredients: [
-      { item: 'mysticalagradditions:dragon_scale' },
-      { item: 'mysticalagradditions:insanium_block' },
-      { item: 'mysticalagradditions:dragon_scale' },
-      { item: 'mysticalagradditions:insanium_block' },
-      { item: 'mysticalagradditions:dragon_scale' },
-      { item: 'mysticalagradditions:insanium_block' },
-      { item: 'mysticalagradditions:dragon_scale' },
-      { item: 'mysticalagradditions:insanium_block' }
+      { item: "mysticalagradditions:dragon_scale" },
+      { item: "mysticalagradditions:insanium_block" },
+      { item: "mysticalagradditions:dragon_scale" },
+      { item: "mysticalagradditions:insanium_block" },
+      { item: "mysticalagradditions:dragon_scale" },
+      { item: "mysticalagradditions:insanium_block" },
+      { item: "mysticalagradditions:dragon_scale" },
+      { item: "mysticalagradditions:insanium_block" },
     ],
-    result: { item: 'kubejs:magical_soil' }
-  })
+    result: { item: "kubejs:magical_soil" },
+  });
 
   //MA EXP droplets to fluid EXP
   event.custom({
     type: "thermal:centrifuge",
     ingredient: {
-      item: "mysticalagriculture:experience_droplet"
+      item: "mysticalagriculture:experience_droplet",
     },
     result: [
       {
         fluid: "cofh_core:experience",
-        amount: 250
-      }
+        amount: 250,
+      },
     ],
-    energy: 400
-  })
+    energy: 400,
+  });
 
   //remove gaia crux
-  //event.remove({ id: "mysticalagradditions:gaia_spirit_crux" })
-})
+  //event.remove({ id: "mysticalagradditions:gaia_spirit_crux" });
+});
--- kubejs/server_scripts/postUnify/ingots.js
+++ kubejs/server_scripts/postUnify/ingots.js
@@ -2,134 +2,168 @@
 // Written by EnigmaQuip as a post almost unified recipe generation script for missing recipes
 
 // Missing tags for unify
-ServerEvents.tags('item', event => {
-  ['falsite', 'ventium', 'horizonite'].forEach(metal => {
-    event.add(`forge:ingots/${metal}`, `blue_skies:${metal}_ingot`)
-  })
-})
+// ServerEvents.tags('item', event => {
+//   ['falsite', 'ventium', 'horizonite'].forEach(metal => {
+//     event.add(`forge:ingots/${metal}`, `blue_skies:${metal}_ingot`)
+//   })
+// })
 
-ServerEvents.recipes(event => {
+ServerEvents.recipes((event) => {
   if (global.devLogging) {
-    console.log('Finishing Unifying on Ingots')
+    console.log("Finishing Unifying on Ingots");
   }
   let ingotTags = global.auTags.filter(function (val) {
-    return /forge:ingots/.test(val)
-  })
+    return /forge:ingots/.test(val);
+  });
   let ingotCount = {
     ie: 0,
-    thermal: 0
-  }
-  ingotTags.forEach(ingotTagString => {
-    let material = ingotTagString.replace('forge:ingots/', '')
-    let ingot = AlmostUnified.getPreferredItemForTag(ingotTagString)
+    thermal: 0,
+  };
+  ingotTags.forEach((ingotTagString) => {
+    let material = ingotTagString.replace("forge:ingots/", "");
+    let ingot = AlmostUnified.getPreferredItemForTag(ingotTagString);
     if (ingot.isEmpty()) {
-      console.log(`${material} does not have a ingot tag entry`)
-      return
+      console.log(`${material} does not have a ingot tag entry`);
+      return;
     }
-    let isAlloy = global.alloys.includes(material)
-    let storageTag = Ingredient.of(`#forge:storage_blocks/${material}`)
-    if (!AlmostUnified.getPreferredItemForTag(`forge:storage_blocks/${material}`).isEmpty()) {
+    let isAlloy = global.alloys.includes(material);
+    let storageTag = Ingredient.of(`#forge:storage_blocks/${material}`);
+    if (
+      !AlmostUnified.getPreferredItemForTag(
+        `forge:storage_blocks/${material}`
+      ).isEmpty()
+    ) {
       if (global.loaded.IE_Loaded) {
         // Check if ie metal press recipe exists and add it if not
-        let count = 0
-        event.forEachRecipe({ type: 'immersiveengineering:metal_press' }, recipe => {
-          let recipeJson = recipe.json
-          let result = recipeJson.get('result')
-          if (result.has('base_ingredient')) {
-            if (global.ingredientCheck(ingot, result.get('base_ingredient'))) {
-              count++
+        let count = 0;
+        event.forEachRecipe(
+          { type: "immersiveengineering:metal_press" },
+          (recipe) => {
+            let recipeJson = recipe.json;
+            let result = recipeJson.get("result");
+            if (result.has("base_ingredient")) {
+              if (
+                global.ingredientCheck(ingot, result.get("base_ingredient"))
+              ) {
+                count++;
+              }
+            } else if (global.ingredientCheck(ingot, result)) {
+              count++;
             }
-          } else if (global.ingredientCheck(ingot, result)) {
-            count++
           }
-        })
+        );
         if (count == 0) {
           let recipe = {
-            type: 'immersiveengineering:metal_press',
-            mold: 'immersiveengineering:mold_unpacking',
+            type: "immersiveengineering:metal_press",
+            mold: "immersiveengineering:mold_unpacking",
             input: storageTag.toJson(),
             result: ingot.withCount(9).toJson(),
-            energy: 2400
-          }
-          event.custom(recipe).id(`kubejs:immersiveengineering/metalpress/unpacking/block_${material}`)
-          ingotCount.ie++
+            energy: 2400,
+          };
+          event
+            .custom(recipe)
+            .id(
+              `kubejs:immersiveengineering/metalpress/unpacking/block_${material}`
+            );
+          ingotCount.ie++;
         }
       }
       if (global.loaded.Thermal_Loaded) {
         // Check if thermal multiservo press recipe exists and add it if not
-        let count = 0
-        event.forEachRecipe({ type: 'thermal:press' }, recipe => {
-          let recipeJson = recipe.json
-          recipeJson.get('result').forEach(item => {
+        let count = 0;
+        event.forEachRecipe({ type: "thermal:press" }, (recipe) => {
+          let recipeJson = recipe.json;
+          recipeJson.get("result").forEach((item) => {
             if (ingot.equalsIgnoringCount(Item.of(item))) {
-              count++
+              count++;
             }
-          })
-        })
+          });
+        });
         if (count == 0) {
-          event.custom({
-            type: 'thermal:press',
-            ingredients: [
-              storageTag.toJson(),
-              Ingredient.of('thermal:press_unpacking_die').toJson(),
-            ],
-            result: [ingot.withCount(9).toJson()],
-          }).id(`kubejs:thermal/machines/press/unpacking/press_${material}_unpacking`)
-          ingotCount.thermal++
+          event
+            .custom({
+              type: "thermal:press",
+              ingredients: [
+                storageTag.toJson(),
+                Ingredient.of("thermal:press_unpacking_die").toJson(),
+              ],
+              result: [ingot.withCount(9).toJson()],
+            })
+            .id(
+              `kubejs:thermal/machines/press/unpacking/press_${material}_unpacking`
+            );
+          ingotCount.thermal++;
         }
       }
     }
-    let nuggetTag = Ingredient.of(`#forge:nuggets/${material}`)
-    if (!AlmostUnified.getPreferredItemForTag(`forge:nuggets/${material}`).isEmpty()) {
+    let nuggetTag = Ingredient.of(`#forge:nuggets/${material}`);
+    if (
+      !AlmostUnified.getPreferredItemForTag(
+        `forge:nuggets/${material}`
+      ).isEmpty()
+    ) {
       if (global.loaded.IE_Loaded) {
         // Check if ie metal press recipe exists and add it if not
-        let count = 0
-        event.forEachRecipe({ type: 'immersiveengineering:metal_press' }, recipe => {
-          let recipeJson = recipe.json
-          let result = recipeJson.get('result')
-          if (result.has('base_ingredient')) {
-            if (global.ingredientCheck(ingot, result.get('base_ingredient'))) {
-              count++
+        let count = 0;
+        event.forEachRecipe(
+          { type: "immersiveengineering:metal_press" },
+          (recipe) => {
+            let recipeJson = recipe.json;
+            let result = recipeJson.get("result");
+            if (result.has("base_ingredient")) {
+              if (
+                global.ingredientCheck(ingot, result.get("base_ingredient"))
+              ) {
+                count++;
+              }
+            } else if (global.ingredientCheck(ingot, result)) {
+              count++;
             }
-          } else if (global.ingredientCheck(ingot, result)) {
-            count++
           }
-        })
+        );
         if (count == 0) {
-          event.custom({
-            type: 'immersiveengineering:metal_press',
-            mold: 'immersiveengineering:mold_packing_9',
-            input: {
-              count: 9,
-              base_ingredient: nuggetTag.toJson()
-            },
-            result: ingot.toJson(),
-            energy: 2400
-          }).id(`kubejs:immersiveengineering/metalpress/packing3x3/${material}_nugget`)
-          ingotCount.ie++
+          event
+            .custom({
+              type: "immersiveengineering:metal_press",
+              mold: "immersiveengineering:mold_packing_9",
+              input: {
+                count: 9,
+                base_ingredient: nuggetTag.toJson(),
+              },
+              result: ingot.toJson(),
+              energy: 2400,
+            })
+            .id(
+              `kubejs:immersiveengineering/metalpress/packing3x3/${material}_nugget`
+            );
+          ingotCount.ie++;
         }
       }
       if (global.loaded.Thermal_Loaded) {
         // Check if thermal multiservo press recipe exists and add it if not
-        let count = 0
-        event.forEachRecipe({ type: 'thermal:press' }, recipe => {
-          let recipeJson = recipe.json
-          recipeJson.get('result').forEach(item => {
+        let count = 0;
+        event.forEachRecipe({ type: "thermal:press" }, (recipe) => {
+          let recipeJson = recipe.json;
+          recipeJson.get("result").forEach((item) => {
             if (ingot.equalsIgnoringCount(Item.of(item))) {
-              count++
+              count++;
             }
-          })
-        })
+          });
+        });
         if (count == 0) {
-          event.custom({
-            type: 'thermal:press',
-            ingredients: [
-              nuggetTag.withCount(9).toJson(),
-              Ingredient.of('thermal:press_packing_3x3_die').toJson(),
-            ],
-            result: [ingot.toJson()],
-          }).id(`kubejs:thermal/machines/press/packing3x3/press_${material}_nugget_packing`)
-          ingotCount.thermal++
+          event
+            .custom({
+              type: "thermal:press",
+              ingredients: [
+                nuggetTag.withCount(9).toJson(),
+                Ingredient.of("thermal:press_packing_3x3_die").toJson(),
+              ],
+              result: [ingot.toJson()],
+            })
+            .id(
+              `kubejs:thermal/machines/press/packing3x3/press_${material}_nugget_packing`
+            );
+          ingotCount.thermal++;
         }
       }
     }
@@ -154,9 +188,11 @@ ServerEvents.recipes(event => {
       thermal induction smelter
       */
     }
-  })
+  });
   if (global.devLogging) {
-    console.log(`Added Ingot Recipes - IE: ${ingotCount.ie}, Thermal: ${ingotCount.thermal}`)
+    console.log(
+      `Added Ingot Recipes - IE: ${ingotCount.ie}, Thermal: ${ingotCount.thermal}`
+    );
     // Added Ingot Recipes - IE: 68, Thermal: 0
   }
-})
+});
