a89e8c5f9767afd10c779e63b857576b67341294
André Augusto <andreaugustoqueiroz999@gmail.com>
2023-03-24 21:29:18 -0300

    ⬆️ update mods
        🔷 Construction Wand
        🔷 Philip's Ruins
        🔷 SuperMartijn642's Core Lib
        🔷 Visual Workbench
    🔨 add refresh after update

--- update.ps1
+++ update.ps1
@@ -2,11 +2,13 @@ Write-Host "Updating client side mods" -ForegroundColor Yellow
 echo ""
 cd ./client
 packwiz update --all
+packwiz refresh
 echo ""
 Write-Host "Updating server side mods" -ForegroundColor Yellow
 echo ""
 cd ../server
 packwiz update --all
+packwiz refresh
 cd ..
 echo ""
 Write-Host "Update finished" -ForegroundColor Green
